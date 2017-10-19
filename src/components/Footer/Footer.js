74c89bd6609e950fd8fbcf8f35a186139596cda7
michaelc.pittman <michaelc.pittman@gmail.com>
2017-10-19 18:42:22 -0400

    test push

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="app-footer">
+        <span><a href="http://coreui.io">AvocadoToast</a> &copy; 2017 GWLabs.</span>
+        <span className="ml-auto">Powered by <a href="http://coreui.io">AvocadoToast</a></span>
+      </footer>
+    )
+  }
+}
+
+export default Footer;
